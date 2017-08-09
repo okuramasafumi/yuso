ea122118bb4df56f8c52f6e108b342ae587dc707
Okura Masafumi <masafumi.o1988@gmail.com>
2017-08-09 16:13:41 +0900

    Change ShippingFee to use Mapping class

--- lib/yuso/sagawa/shipping_fee.rb
+++ lib/yuso/sagawa/shipping_fee.rb
@@ -1,14 +1,19 @@
 require "json"
+require_relative "./mapping"
 
 module Yuso
   class Sagawa
     class ShippingFee
       def self.load(prefecture_name)
-        mapping_path = Yuso::DATA_PATH + "/sagawa/mapping.json"
-        mapping = JSON.load(File.read(mapping_path))
         area = mapping.fetch(prefecture_name) { raise ArgumentError, 'Prefecture name is not correct.' }
         json_path = Yuso::DATA_PATH + "/sagawa/#{area}.json"
         JSON.load(File.read(json_path))
+      end
+
+      private
+
+      def self.mapping
+        Mapping.load
       end
     end
   end
--- lib/yuso/yamato/shipping_fee.rb
+++ lib/yuso/yamato/shipping_fee.rb
@@ -1,14 +1,19 @@
 require "json"
+require_relative "./mapping"
 
 module Yuso
   class Yamato
     class ShippingFee
       def self.load(prefecture_name)
-        mapping_path = Yuso::DATA_PATH + "/yamato/mapping.json"
-        mapping = JSON.load(File.read(mapping_path))
         area = mapping.fetch(prefecture_name) { raise ArgumentError, 'Prefecture name is not correct.' }
         json_path = Yuso::DATA_PATH + "/yamato/#{area}.json"
         JSON.load(File.read(json_path))
+      end
+
+      private
+
+      def self.mapping
+        Mapping.load
       end
     end
   end
