4bc95f891ac7e7c623174ec78ede0cff6bacb00d
Okura Masafumi <masafumi.o1988@gmail.com>
2017-12-08 17:12:28 +0900

    Change inner char of period in delivery_time_periods
    From `~` to `-`

--- lib/yuso/yubin.rb
+++ lib/yuso/yubin.rb
@@ -3,12 +3,12 @@ module Yuso
   class Yubin
     def self.delivery_time_periods
       [
-        DeliveryTimePeriod.new(company: 'yubin', period: '08:00 ~ 12:00'),
-        DeliveryTimePeriod.new(company: 'yubin', period: '12:00 ~ 14:00'),
-        DeliveryTimePeriod.new(company: 'yubin', period: '14:00 ~ 16:00'),
-        DeliveryTimePeriod.new(company: 'yubin', period: '16:00 ~ 18:00'),
-        DeliveryTimePeriod.new(company: 'yubin', period: '18:00 ~ 20:00'),
-        DeliveryTimePeriod.new(company: 'yubin', period: '20:00 ~ 21:00')
+        DeliveryTimePeriod.new(company: 'yubin', period: '08:00 - 12:00'),
+        DeliveryTimePeriod.new(company: 'yubin', period: '12:00 - 14:00'),
+        DeliveryTimePeriod.new(company: 'yubin', period: '14:00 - 16:00'),
+        DeliveryTimePeriod.new(company: 'yubin', period: '16:00 - 18:00'),
+        DeliveryTimePeriod.new(company: 'yubin', period: '18:00 - 20:00'),
+        DeliveryTimePeriod.new(company: 'yubin', period: '20:00 - 21:00')
       ]
     end
   end
